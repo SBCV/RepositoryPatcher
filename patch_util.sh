ec040dd53c25e6543aa15fc35136c100897ed5dc
Sebastian Bullinger <sebastian.bullinger@iosb.fraunhofer.de>
2025-02-12 11:26:01 +0100

    Introduce proper options and restructure

--- patch_util.sh
+++ patch_util.sh
@@ -6,13 +6,16 @@ else
 fi
 
 get_patch_files_as_array() {
-    local patch_dir="$1"
-    patch_files_as_string=$(find "$patch_dir" -type f -name "*.patch")
-    mapfile -t patch_files_as_array <<< $patch_files_as_string
+    local _patch_dir="$1"
+    local _patch_files_as_string=$(find "$_patch_dir" -type f -name "*.patch")
+    mapfile -t patch_files_as_array <<< $_patch_files_as_string
     echo "${patch_files_as_array[@]}"
 }
 
 apply_patch() {
+    local _tool=$1
+    local _option=$2
+    local _patch_fp=$3
     # Options:
     #  "-v"         Verbose (useful for debugging, shows why applying patch failed)
     #  "--reject"   Creation of *.rej files (hunks that failed to apply) 
@@ -20,81 +23,97 @@ apply_patch() {
     #               less strict matching of context lines.
     # Note: The "--reject" and "--3way" options can not be used together
 
-    if [ $TOOL == "git_apply" ]; then
-        git apply $OPTIONS $PATCH
-    elif [ $TOOL == "patch" ]; then
-        # --reject-format=FORMAT  Create 'context' or 'unified' rejects.
-        if [ $OPTIONS == "--reject" ]; then
-            # Reject descibes the default behavior of patch, so ommit any parameter
-            patch -p1 -i $PATCH
-        elif [ $OPTIONS == "--merge" ]; then
-            patch -p1 --merge -i $PATCH
+    if [ $_tool == "git_apply" ]; then
+        if [ $_option == "reject" ]; then
+            git apply --reject "$_patch_fp"
+        elif [ $_option == "3way" ]; then
+            git apply --3way "$_patch_fp"
         else
-            echo "SOMETHING WENT TERRIBLY WRONG"
+            echo "SOMETHING WENT WRONG. Invalid option: $_option"
+            exit 1
+        fi
+    elif [ $_tool == "patch" ]; then
+        # --reject-format=FORMAT  Create 'context' or 'unified' rejects.
+        if [ $_option == "--reject" ]; then
+            # Reject descibes the default behavior of patch, so ommit any parameter
+            patch -p1 -i $_patch_fp
+        elif [ $_option == "--merge" ]; then
+            patch -p1 --merge -i $_patch_fp
+        else
+            echo "SOMETHING WENT WRONG. Invalid option: $_option"
             exit 1
         fi
     else
-        echo "SOMETHING WENT TERRIBLY WRONG"
+        echo "SOMETHING WENT WRONG. Invalid tool: $_tool"
         exit 1
     fi
 
     # Check the exit status of git apply
     if [ $? -ne 0 ]; then
         # Return 1 if patch fails
-        echo "Failed to apply patch: $PATCH"
+        echo "Failed to apply patch: $_patch_fp"
         return 1
     fi
     return 0
 }
 
 apply_patch_lazy() {
+    local _tool=$1
+    local _option=$2
+    local _patch_fp=$1
+
     # Read the file names affected by the patch file
-    RELATIVE_FP_PAIR=$(grep -oP '(?<=a/).*' <<< $(grep -m 1 -oP 'a/.*' $PATCH))
+    local _relative_fp_pair=$(grep -oP '(?<=a/).*' <<< $(grep -m 1 -oP 'a/.*' $_patch_fp))
     # Get the first file name
-    RELATIVE_FP=$(echo $RELATIVE_FP_PAIR | cut -d' ' -f1)
+    local _relative_fp=$(echo $_relative_fp_pair | cut -d' ' -f1)
     # Get the absolute path of the file, the file is located in $COLMAP_TARGET_DP
-    ABSOLUTE_FP="./${RELATIVE_FP}"
+    local _absolute_fp="./${_relative_fp}"
 
-    ABSOLUTE_TEMP_FP="$ABSOLUTE_FP.tmp"
-    mv $ABSOLUTE_FP $ABSOLUTE_TEMP_FP
+    local _absolute_temp_fp="$_absolute_fp.nnj_tmp"
+    mv $_absolute_fp $_absolute_temp_fp
 
-    git checkout $ABSOLUTE_FP > /dev/null 2>&1
-    apply_patch
-    APPLY_RESULT=$?
-    if [ "$APPLY_RESULT" -eq 1 ]; then
+    git checkout $_absolute_fp > /dev/null 2>&1
+    apply_patch $_tool $_option $_patch_fp
+    local _apply_result=$?
+    if [ "$_apply_result" -eq 1 ]; then
         return 1
     fi
 
-    # Check if ABSOLUTE_TEMP_FP and ABSOLUTE_FP are different
-    diff $ABSOLUTE_FP $ABSOLUTE_TEMP_FP > /dev/null
+    # Check if _absolute_temp_fp and absolute_fp are different
+    diff $_absolute_fp $_absolute_temp_fp > /dev/null
     if [ $? -eq 0 ]; then
         # Files are identical, move restore old file (to maintain the metadata)
-        mv $ABSOLUTE_TEMP_FP $ABSOLUTE_FP
+        mv $_absolute_temp_fp $_absolute_fp
     fi
     return 0
 }
 
 
 apply_patches() {
-    local lazy=$1
-    local target_repository_dp=$2
-    # Shift the parameters to process only the patch files
-    shift 2
-    local patches=("$@")
+    local _tool=$1
+    local _option=$2
+    local _target_repository_dp=$3
+    local _patch_dp=$4
+    local _lazy=$5
+    local _patch_files_as_array=($(get_patch_files_as_array "$_patch_dp"))
+    # for _patch_file in "${_patch_files_as_array[@]}"; do
+    #     echo "$_patch_file"
+    # done
 
-    cd $target_repository_dp
+    cd $_target_repository_dp
 
     # Loop through each patch file in the the patch file array
-    for PATCH in "$@"; do
-        APPLY_RESULT=-1
-        if [ $lazy = 1 ]; then
-            apply_patch_lazy
-            APPLY_RESULT=$?
+    local _patch_fp=""
+    for _patch_fp in "${_patch_files_as_array[@]}"; do
+        local _apply_result=-1
+        if [ $_lazy = 1 ]; then
+            apply_patch_lazy $_tool $_option $_patch_fp
+            _apply_result=$?
         else
-            apply_patch
-            APPLY_RESULT=$?
+            apply_patch $_tool $_option $_patch_fp
+            _apply_result=$?
         fi
-        if [ "$APPLY_RESULT" -eq 1 ]; then
+        if [ "$_apply_result" -eq 1 ]; then
             exit 1
         fi
     done
